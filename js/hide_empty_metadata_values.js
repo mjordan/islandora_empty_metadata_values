d1ad9f8c7b90e1cca87bf8e9a33dfb8f3244d7dc
Mark Jordan <mjordan@sfu.ca>
2016-07-13 14:23:41 +0000

    Address #1.

--- js/hide_empty_metadata_values.js
+++ js/hide_empty_metadata_values.js
@@ -1,6 +1,8 @@
 (function ($) {
   $(document).ready(function() {
-    $("dl.islandora-metadata-fields > dd").each(function() {
+    // dl class islandora-metadata-fields is used in metadatata displays,
+    // class solr-fields in search results.
+    $("dl.islandora-metadata-fields, dl.solr-fields > dd").each(function() {
       // Value of the <dd> cannot be emtpy or contain only puncutation.
       if ($.trim($(this).text()) == '' || $.trim($(this).text()).match(/^[.,;:\-]$/)) {
         // Hide the <dd>.
